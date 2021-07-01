3d9f8065e0388e1d59e1e6c0efd912be770c80ac
Denis Dizdarevic <denis.dizdarevic@fpmoz.sum.ba>
2021-07-01 16:29:56 +0200

    dodao blog app, modele, drf router

---
+++ blog/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from shortuuid import uuid
+
+
+class Post(models.Model):
+    title = models.TextField()
+    slug = models.SlugField(unique=True, null=False)
+    description = models.TextField()
+    content = models.TextField()
+    author = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL)
+    timestamp_created = models.DateTimeField(auto_now_add=True)
+    timestamp_edited = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = uuid()
+        super().save(*args, **kwargs)
+
+
+class Tag(models.Model):
+    name = models.TextField()
+    post = models.ForeignKey(Post, related_name='tags',  on_delete=models.CASCADE)
+
+
+class Attachment(models.Model):
+    file = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    post = models.ForeignKey(Post, related_name='attachments', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+
+
+class Like(models.Model):
+    author = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
--- server/settings.py
+++ server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'PAGINATE_BY_PARAM': 'limit'
+}
