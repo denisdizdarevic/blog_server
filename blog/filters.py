cf03ce03fbc16db64eb5a73e4924b9cba22a44f8
Denis Dizdarevic <denis.dizdarevic@fpmoz.sum.ba>
2021-07-11 20:29:11 +0200

    popravio tag filter

--- blog/filters.py
+++ blog/filters.py
@@ -1,9 +1,15 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, ModelMultipleChoiceFilter
+from taggit.models import Tag
 
 from blog.models import Post
 
 
 class PostFilter(FilterSet):
+    tag = ModelMultipleChoiceFilter(conjoined=True,
+                                    field_name="tags__name",
+                                    to_field_name="name",
+                                    queryset=Tag.objects.all())
+
     class Meta:
         model = Post
         fields = {
@@ -11,5 +17,5 @@ class PostFilter(FilterSet):
             'description': ['contains'],
             'author': ['exact'],
             'timestamp_created': ['date__gte', 'date__lte'],
-            'tags__name': ['exact', 'in']
+            # 'tags__name': ['exact']
         }
