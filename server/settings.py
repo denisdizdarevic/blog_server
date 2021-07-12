87707f0589f66746e06d5bf3ab44621adc701a1b
Denis Dizdarevic <denis.dizdarevic@fpmoz.sum.ba>
2021-07-12 10:27:38 +0200

    obrisao package koji se više ne koristi

--- server/settings.py
+++ server/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'admin_permissions',
     'django_summernote',
     'taggit',
     'taggit_labels',
